7f3e1478ca1525dd931217ee131d13bf097226b4
aboroch <ariel.borochov@sscinc.com>
2021-01-23 18:07:13 -0500

    remove unneeded console statments

--- functions/rss.js
+++ functions/rss.js
@@ -4,22 +4,8 @@ let parser = new Parser();
 function getActivity(rssFeedObject, days ){
     console.log("getActivity")
     keys = Object.keys(rssFeedObject)
-    console.log(keys)
     let date_ob = new Date();
     keys.forEach( function (key){
-        console.log(key)
-        console.log(rssFeedObject[key])
-    //     (async () => {
-
-    // //         let feed = await parser.parseURL(rssFeedObject[key]);
-    // //         console.log(feed.title);
-          
-    // //         feed.items.forEach(item => {
-    // //         //   console.log(item.title + ':' + item.link)
-    // //         });
-          
-    //       })();
-    // let parser = new RSSParser();
         parser.parseURL(rssFeedObject[key], function(err, feed) {
         if (err) throw err;
         console.log(feed.title);
