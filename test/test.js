ee5b97eaae209e329c6cfef4527b6e1be91ce79f
aboroch <ariel.borochov@sscinc.com>
2021-01-24 08:15:15 -0500

    test are working for case #1 postive case

--- test/test.js
+++ test/test.js
@@ -1,12 +1,25 @@
 const request = require('supertest');
+const chai = require('chai')
 const app = require('../app.js');
+should = chai.should();
 
-describe('GET /api/v1/companies', function() {
+
+describe('check endpoints endpoint ', function() {
   it('return json response', function() {
     return request(app)
       .get('/api/v1/companies')
       .expect(200)
       .expect('Content-Type',/json/)
       .expect('{"users":[{"name":"Timmy"}]}')
+  })
+
+  it('returns json response for post', function () {
+    return request(app)
+    .post('/api/v1/companies')
+    .set("Content-Type", "application/json")
+    .send({rss: { joerogan: "http://joeroganexp.joerogan.libsynpro.com/rss" }})
+    .expect(200)
+    .expect('Content-Type',/json/)
+    .expect('{"results":["joerogan"]}')
   })
 })
